1f99dde70e1fc587bed505566296a8b21223e69e
jlhuerta <jlhuerta@mx1.ibm.com>
2016-08-19 10:00:31 -0500

    Business Partner module was added

---
+++ gateway-ui/src/app/components/configure-business-partner/configure-business-partner.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import {BusinessPartnerService} from "../../common/services/business-partner.service";
+import {IBusinessPartner} from "../../common/domain/IBusinessPartner";
+import { DataTable, Column, Header, Footer, PanelMenu } from 'primeng/primeng';
+
+@Component({
+  moduleId: module.id,
+  selector: 'app-configure-business-partner',
+  templateUrl: 'configure-business-partner.component.html',
+  directives: [ DataTable, Column, Header, Footer, PanelMenu ]
+})
+export class ConfigureBusinessPartnerComponent implements OnInit {
+
+  businessPartners: IBusinessPartner[];
+
+  constructor(private businessPartnerService: BusinessPartnerService) {
+  }
+
+  ngOnInit(){
+    this.businessPartnerService.getBusinessPartners()
+      .subscribe(
+        data => this.businessPartners = data,
+        error => alert(error),
+        () => console.log(this.businessPartners)
+      );
+  }
+
+}
---
+++ gateway-ui/src/app/components/configure-business-partner/index.ts
@@ -0,0 +1,1 @@
+export * from './configure-business-partner.component';
